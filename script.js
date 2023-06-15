5c8e861a5ced7011cae98363a9e659da3a044e69
Lucas <lucasclaudinocc@gmail.com>
2023-06-14 23:34:40 -0300

    Added button reactivity

---
+++ script.js
@@ -0,0 +1,71 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice () {
+    let choiceNumber = Math.floor(Math.random() * 3);
+
+    if (choiceNumber === 0) {
+        choice = "Rock";
+ 
+    }
+
+    if (choiceNumber === 1) {
+        choice = "Paper";
+     
+
+    }
+
+    if (choiceNumber === 2) {
+        choice = "Scissors";
+        
+    }
+
+    return choice;
+}
+
+function getPlayerChoice () {
+    let bruteAnswer = prompt("Rock, Paper or Scissors?");
+    let postAnswer = bruteAnswer.toLowerCase();
+    let answer = postAnswer[0].toUpperCase() + postAnswer.substring(1);
+    return answer;
+}
+
+// let computerSelection = getComputerChoice();
+
+
+function play (playerSelection, computerSelection) {
+   
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+
+
+    if (computerSelection === playerSelection) {
+        alert("Tie. You and the computer choose " + playerSelection + "!");
+    }
+
+    if ((computerSelection === "Rock" && playerSelection === "Scissors") || 
+    (computerSelection === "Scissors" && playerSelection === "Paper") || 
+    (computerSelection === "Paper" && playerSelection === "Rock")) {
+        alert("You lose. " + computerSelection + " beats " + playerSelection);
+        computerScore++;
+    }
+
+    if ((computerSelection === "Rock" && playerSelection === "Paper") || 
+    (computerSelection === "Scissors" && playerSelection === "Rock") || 
+    (computerSelection === "Paper" && playerSelection === "Scissors")) {
+        alert("You win! " + playerSelection + " beats " + computerSelection + "!");
+        playerScore++;
+    }
+
+    return;
+
+}
+
+function game() {
+ for (i = 0; playerScore < 5 && computerScore < 5; i++) {
+    play();
+ }
+}
+
+game();
